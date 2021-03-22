69f4b713210625271827f76648f0414a02edadce
StanC666 <67333902+StanC666@users.noreply.github.com>
2021-03-22 21:49:13 +0300

    Created first test file

---
+++ test1.ts
@@ -0,0 +1,9 @@
+let a: number = 5;
+//Note:- while testing, this code is to be modified to
+//Exclude this comment section
+//Note2:- the position of the colon must not matter.
+// let a :number = 5;
+// Should be treated the same way as 
+// let a: number = 5;
+// Semicolons at the end of statements are made compulsory 
+// For now
