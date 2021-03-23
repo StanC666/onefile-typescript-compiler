21a0e92746aa6d0a9cff7d83172f21a1c9ba9d65
StanC666 <star.gamer.6886@gmail.com>
2021-03-23 16:00:09 +0300

    Added filename acceptor

--- main.c
+++ main.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
 int main(int argc, char* argv[]) {
-  return 0;
+	FILE* fp = fopen(argv[1], "r");
+	if (fp == NULL) {
+		printf("OTC > error: No such file exists as %s\n", argv[1]);
+		return -1;
+	}
+	
+ 	return 0;
 }
